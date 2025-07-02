af1d0de927eed1c342acda5a51438336bbf7faff
gpelincel <gabrielpelincel@gmail.com>
2025-07-02 17:43:16 -0300

    Feat: Icons e Splash Screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.descarmed.os.app',
   appName: 'DescarmedOS',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,       // em milissegundos (3s)
+      backgroundColor: "#00000000",   // cor de fundo no formato ARGB (ex.: branco)
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true
+    }
+  }
 };
 
 export default config;
